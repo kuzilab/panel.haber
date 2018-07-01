180e7c70661bd89870132808a25459e218b37352
kuzilab <39768869+kuzilab@users.noreply.github.com>
2018-07-01 04:16:49 +0300

    panelHaber
    changed port

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    "port": process.env.PORT || 3001,
+    "port": process.env.PORT || 3000,
     "database": "mongodb://hkuzudisli:Hk5967245.@ds053216.mlab.com:53216/nodedb"
 
 }
